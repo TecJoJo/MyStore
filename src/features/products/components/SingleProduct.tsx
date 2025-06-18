ae1c06723185da89285149ec24e84c9eef7d2cec
Yao Laptop <luyao2493751@gmail.com>
2025-06-18 23:41:49 +0300

    hover effect without using useState

--- src/features/products/components/SingleProduct.tsx
+++ src/features/products/components/SingleProduct.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react"
-
 interface Props {
   id?: string
   name: string
@@ -19,26 +17,12 @@ function SingleProduct({
   category,
   stock,
 }: Props) {
-  const [isTextShown, setIsTextShown] = useState(false)
-
   return (
-    <div
-      className="w-1/4 relative flex items-center justify-center cursor-pointer overflow-hidden"
-      onMouseEnter={() => {
-        setIsTextShown(true)
-      }}
-      onMouseLeave={() => {
-        setIsTextShown(false)
-      }}
-    >
+    <div className="w-1/4 relative flex items-center justify-center cursor-pointer overflow-hidden group">
       <div>
         <img src={imageUrl} alt={name} className="object-cover" />
-        <div
-          className={`w-full h-full absolute top-0 left-0 bg-white ${isTextShown ? "opacity-80" : "opacity-0"} z-10 transition-opacity duration-300`}
-        ></div>
-        <div
-          className={`w-full h-full absolute top-0 left-0 font-mono font-semibold text-9xl text-zinc-900 flex flex-col items-center justify-center z-20 ${isTextShown ? "opacity-100" : "opacity-0"} transition-opacity duration-300`}
-        >
+        <div className="w-full h-full absolute top-0 left-0 bg-white opacity-0 group-hover:opacity-80 z-10 transition-opacity duration-300"></div>
+        <div className="w-full h-full absolute top-0 left-0 font-mono font-semibold text-9xl text-zinc-900 flex flex-col items-center justify-center z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           <p>{name}</p>
           <p>${price}</p>
         </div>
