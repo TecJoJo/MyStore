decd876cd6b4f1b29c4d9c3092601d235d449a43
Yao Lu Home Win11 <luyao2493751@gmail.com>
2025-06-22 22:38:44 +0300

    - Provide auth header with jwt token for fetching products API call 	- Conditionally render context of the products

--- src/api/Products/getAllProductsApiRequest.ts
+++ src/api/Products/getAllProductsApiRequest.ts
@@ -1,13 +1,22 @@
 import axios from "axios"
-import { GetAllProductsResponseDto } from "./models";
-import { urls } from "../common/models";
+import { GetAllProductsResponseDto } from "./models"
+import { urls } from "../common/models"
 
-export const getAllProductsApiRequest = async (): Promise<GetAllProductsResponseDto> => {
+export const getAllProductsApiRequest =
+  async (): Promise<GetAllProductsResponseDto> => {
     try {
-        const response = await axios.get<GetAllProductsResponseDto>(urls.getAllProducts);
-        return response.data;
+      const token = localStorage.getItem("jwtToken")
+      const response = await axios.get<GetAllProductsResponseDto>(
+        urls.getAllProducts,
+        {
+          headers: {
+            Authorization: token ? `Bearer ${token}` : "",
+          },
+        },
+      )
+      return response.data
     } catch (error) {
-        console.error("Error fetching all products:", error);
-        throw error;
+      console.error("Error fetching all products:", error)
+      throw error
     }
-};
+  }
--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -6,13 +6,17 @@ import SingleProduct from "./components/SingleProduct"
 function Products() {
   const dispatch = useAppDispatch()
   const allProducts = useAppSelector(state => state.products.products)
+  const userLoginState = useAppSelector(
+    state => state.authentication.loginState,
+  )
+  const isUserLoggedIn = userLoginState === "succeeded"
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     dispatch(getAllProducts())
   }, [dispatch])
 
-  return (
+  return isUserLoggedIn ? (
     <div className="w-full my-4 md:my-8 md:w-10/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 justify-items-stretch">
       {allProducts.map(product => (
         <SingleProduct
@@ -23,6 +27,16 @@ function Products() {
           imageUrl={product.imageUrl}
         />
       ))}
+    </div>
+  ) : (
+    <div>
+      <p className="my-8 md:my-16 w-full text-center font-semibold text-lg font-serif">
+        Please log in to view products
+      </p>
+      <p className="text-orange-700 my-8 md:my-16 w-full text-center font-base text-md font-mono">
+        I know this is not making any sense, but this is just a temporary branch
+        for my E2E test Demo!!!
+      </p>
     </div>
   )
 }
