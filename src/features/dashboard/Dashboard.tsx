0f2589e553a03e2cf4d74d25f8aa749d0a7af704
Yao Lu Home Win11 <luyao2493751@gmail.com>
2025-08-01 22:58:18 +0300

    MUI dashbaord with ability to browse existing products

---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ProductList from "./components/ProductsList"
+
+function Dashboard() {
+  return (
+    <>
+      <div>Dashboard</div>
+      <ProductList />
+    </>
+  )
+}
+
+export default Dashboard
